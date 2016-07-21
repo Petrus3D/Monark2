29c60268749dc19565ab63f8549a3ce35c064f60
Paul Bouquet <paul.bouquet@gmail.com>
2016-07-21 22:42:07 +0200

    2.0.2.05

--- web/js/game/map.js
+++ web/js/game/map.js
@@ -54,12 +54,15 @@ $(document).on("pjax:end", function() {
 // function used to show Modals
 function ShowModalLoading(args){ShowModal();UpdateModalTitle(args);$("#modal-view").find("#modal-view-Content").html(config["text"]["modal_loading_content"]);}
 function UpdateModalTitle(args){if(args['title_with_land_name']){args['title'] = args['land_name'] + " " + args['title'];}$(".modal-header-title").html("<center><h4>"+args['title']+"</h4></center>");}
-function UpdateModalContent(args){$("#modal-view").find("#modal-view-Content").html(args['content']);}
+function UpdateModalContent(args){actionObjectFade($("#modal-view").find("#modal-view-Content"), "html", args['content']);}
 function UpdateModalError(){UpdateModalContent({'content': config["text"]["modal_error_content"]});}
-function ShowModal(){$("#modal-view").modal("show");}
-function HideModal(){$("#modal-view").modal("hide");}
+function ShowModal(){actionObjectFade($("#modal-view"), "modal", "show", 100);}
+function HideModal(){actionObjectFade($("#modal-view"), "modal", "hide", 100);}
 function ModalInfo(){$("#modal-view").addClass("modal modal-info");}
 function ModalError(){$("#modal-view").addClass("modal modal-danger");}
+
+// Animations
+function actionObjectFade(object, action, params="", speed=null){if(speed == null)speed = 'slow';$(object).fadeOut(speed, function() {$(object)[action](params);$(object).fadeIn(speed);});}
 
 // Popover
 function close_popover(id){$(id).popover("hide");}
