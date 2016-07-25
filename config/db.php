fb2213c0113fe35a13b5804a12410ebc3ffef567
Paul Bouquet <paul.bouquet@gmail.com>
2016-07-25 21:49:40 +0200

    Default local config db

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=mk',
-    'username' => 'mk',
-    'password' => 'b&ip8yourpasswordverysecurised&l',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
