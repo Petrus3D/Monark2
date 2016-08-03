d707643f0e7de032261ef774a8d8f419039d0e6e
Paul Bouquet <paul.bouquet@gmail.com>
2016-08-03 22:06:45 +0200

    Update vendors

--- vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/site/login.php
+++ vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/site/login.php
@@ -40,13 +40,12 @@ $fieldOptions2 = [
             ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
 
         <div class="row">
-            <div class="col-xs-4">
-                <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
-            </div>
-            <!-- /.col -->
-
             <div class="col-xs-8">
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            </div>
+            <!-- /.col -->
+            <div class="col-xs-4">
+                <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
             </div>
             <!-- /.col -->
         </div>
@@ -54,13 +53,13 @@ $fieldOptions2 = [
 
         <?php ActiveForm::end(); ?>
 
-        <!--<div class="social-auth-links text-center">
+        <div class="social-auth-links text-center">
             <p>- OR -</p>
             <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in
                 using Facebook</a>
             <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign
                 in using Google+</a>
-        </div>-->
+        </div>
         <!-- /.social-auth-links -->
 
         <a href="#">I forgot my password</a><br>
